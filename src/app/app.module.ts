071e7349feb20ce63c3ab62a83f4099cf6ca01a8
NGDK78 <karol.koson@motorolasolutions.com>
2021-11-28 21:31:35 +0100

    posts-and-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
