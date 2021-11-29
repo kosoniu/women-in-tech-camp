cbea109cca653a7a2771c4cbdb7beb6d6df0979a
NGDK78 <karol.koson@motorolasolutions.com>
2021-11-29 23:05:40 +0100

    post-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PostsComponent} from './posts/posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/posts', pathMatch: 'full' },
+  {
+    path: 'posts',
+    component: PostsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
