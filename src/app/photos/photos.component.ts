f587e716e6652cb9bcab4d7c555a6f5341417ddb
NGDK78 <karol.koson@motorolasolutions.com>
2021-11-29 23:25:25 +0100

    photos-data

--- src/app/photos/photos.component.ts
+++ src/app/photos/photos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-photos',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhotosComponent implements OnInit {
 
-  constructor() { }
+  photos: any[] = [];
+
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
+    this.httpClient.get<any[]>('https://jsonplaceholder.typicode.com/photos')
+      .subscribe(photos => {
+        this.photos = photos.slice(0, 10);
+        console.log(this.photos);
+      });
   }
 
 }
